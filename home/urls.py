9052c3fdd7e8c037b4032dfc3ddf2ad0253b6f87
VrBnDev <rafaelavborgess@gmail.com>
2024-08-21 22:52:26 -0300

    feat(desafios): criação de page desafios para listagem de atividades entregues

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path("", views.home, name="index"),
     path("sobre/", views.sobre, name="sobre"),
     path("contato/", views.contato, name="contato"),
-    path("ajuda/", views.ajuda, name="ajuda"),
+    path("desafio/", views.desafio, name="desafio"),
     path("perfil/<str:usuario>", views.perfil, name='perfil'),
     path("diasemana/<str:dia>", views.diasemana, name='diasemana'),
+    path("lista/", views.lista, name='lista'),
+    path("formularioproduto/", views.formularioproduto, name='formularioproduto')
 ]
--- home/views.py
+++ home/views.py
@@ -14,8 +14,15 @@ def contato(request):
     return render(request, 'contato.html')
 
 
-def ajuda(request):
-    return render(request, 'ajuda.html')
+def desafio(request):
+    contexto = {
+        'lista' :[
+        {'id': 1, 'nome': 'Perfil','descricao': 'Redicione o usuário para uma página com uma saudação e o nome dele.', 'imagem': "perfil.jpg", "input": "inline", 'url': "perfil"},
+        {'id': 2, 'nome': 'Dias da Semana','descricao': 'Caro usuário você tem direito a uma mensagem semanal. Para escolher sua mensagem, escolha um número de 1 a 7.', 'imagem': "diassemanas.png", "input": "inline", 'url': "diasemana"},
+        {'id': 3, 'nome': 'Produtos','descricao': 'Nesse case uma listagem de produtos foi cadastrado.', 'imagem': "produtos.jpg", "input": "none", 'url': "produto"},
+    ]}
+
+    return render(request, 'desafio.html', contexto)
 
 
 def perfil(request, usuario):
@@ -29,3 +36,17 @@ def diasemana(request, dia):
         return render(request, 'diasemana.html', {'dia': 'inválido'})
 
     return render(request, 'diasemana.html', {'dia': semana[dia]})
+
+def lista(request):
+    contexto = {
+        'lista': [
+            {'id': 1, 'nome': 'Notebook', 'preco': '2.500,00'},
+            {'id': 2, 'nome': 'Monitor', 'preco': '500,00'},
+            {'id': 3, 'nome': 'Teclado', 'preco': '80,00'},
+        ]
+    }
+
+    return render(request, 'produto/lista.html', contexto)
+
+def formularioproduto(request):
+    return render(request, 'produto/formulario.html')
